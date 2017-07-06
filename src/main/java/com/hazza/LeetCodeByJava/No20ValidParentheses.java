758a98aa800cef5205a042d69bbc6b8f87a25cfa
HazzaCheng <hazzacheng@gmail.com>
2017-07-06 09:01:40 +0800

    add: No20

---
+++ src/main/java/com/hazza/LeetCodeByJava/No20ValidParentheses.java
@@ -0,0 +1,20 @@
+package com.hazza.LeetCodeByJava;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/valid-parentheses/#/description
+ */
+public class No20ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (Character c: s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') stack.push(c);
+            if (c == ')' && (stack.isEmpty() || stack.pop() != '(')) return false;
+            if (c == ']' && (stack.isEmpty() || stack.pop() != '[')) return false;
+            if (c == '}' && (stack.isEmpty() || stack.pop() != '{')) return false;
+        }
+        if (!stack.isEmpty()) return false;
+        return true;
+    }
+}
