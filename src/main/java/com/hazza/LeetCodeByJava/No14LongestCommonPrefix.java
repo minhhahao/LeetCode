fbe2236de63a80fecb4d48c8dbd9190b03fd93ce
HazzaCheng <hazzacheng@gmail.com>
2017-07-07 19:02:13 +0800

    add: No14

---
+++ src/main/java/com/hazza/LeetCodeByJava/No14LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package com.hazza.LeetCodeByJava;
+
+/**
+ * https://leetcode.com/problems/longest-common-prefix/#/description
+ */
+public class No14LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0)    return "";
+        String pre = strs[0];
+
+        for(int i = 1; i < strs.length; ++i) {
+            int j = 0;
+            for(; j < pre.length(); ++j) {
+                if (j >= strs[i].length() || pre.charAt(j) != strs[i].charAt(j))
+                    break;
+            }
+            pre = pre.substring(0, j);
+        }
+
+        return pre;
+    }
+}
