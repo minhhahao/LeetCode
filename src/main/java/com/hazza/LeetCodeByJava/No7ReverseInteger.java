feeca50b1e70224fbec0da74ebbcc6fff261eb9d
HazzaCheng <hazzacheng@gmail.com>
2017-07-07 18:55:35 +0800

    add: No7

---
+++ src/main/java/com/hazza/LeetCodeByJava/No7ReverseInteger.java
@@ -0,0 +1,20 @@
+package com.hazza.LeetCodeByJava;
+
+/**
+ * https://leetcode.com/problems/reverse-integer/?tab=Description#/description
+ */
+public class No7ReverseInteger {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int tail = x % 10;
+            int newRes = res * 10 + tail;
+            if ((newRes - tail) / 10 != res) {
+                return 0;
+            }
+            res = newRes;
+            x = x / 10;
+        }
+        return res;
+    }
+}
