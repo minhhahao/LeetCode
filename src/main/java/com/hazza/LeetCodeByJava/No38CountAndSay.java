84dd24719a8564005c841a615560f52bcfe6252e
HazzaCheng <hazzacheng@gmail.com>
2017-07-23 09:18:55 +0800

    add: No38

---
+++ src/main/java/com/hazza/LeetCodeByJava/No38CountAndSay.java
@@ -0,0 +1,49 @@
+package com.hazza.LeetCodeByJava;
+
+/**
+ * https://leetcode.com/problems/count-and-say/#/description
+ */
+public class No38CountAndSay {
+
+    //Solution 1
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        String str = countAndSay(n - 1);
+        int count = 1;
+        StringBuilder res = new StringBuilder();
+        char[] cs = str.toCharArray();
+        for (int i = 1, len = cs.length; i < len; i++) {
+            if (cs[i] == cs[i - 1]) ++count;
+            else {
+                res.append(count).append(cs[i - 1]);
+                count = 1;
+            }
+        }
+        res.append(count).append(cs[cs.length - 1]);
+        return res.toString();
+    }
+
+    //Solution 1
+    public String countAndSay2(int n) {
+        StringBuilder curr = new StringBuilder("1");
+        StringBuilder prev = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            prev = curr;
+            curr = new StringBuilder();
+            count = 1;
+            char c = prev.charAt(0);
+            for (int j = 1, len = prev.length(); j < len; j++) {
+                if (prev.charAt(j) != c) {
+                    curr.append(count).append(c);
+                    count = 1;
+                    c = prev.charAt(j);
+                } else ++count;
+            }
+            curr.append(count).append(c);
+        }
+
+        return curr.toString();
+    }
+}
---
+++ src/test/java/com/hazza/LeetCodeByJava/No38Test.java
@@ -0,0 +1,33 @@
+package com.hazza.LeetCodeByJava;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by hazza on 7/23/17.
+ */
+public class No38Test {
+    No38CountAndSay test;
+
+    @Before
+    public void setUp() {
+        test = new No38CountAndSay();
+    }
+
+    @Test
+    public void testMany() {
+        String s1 = "1";
+        String s2 = "11";
+        String s3 = "21";
+        String s4 = "1211";
+        String s5 = "111221";
+        assertEquals(s1, test.countAndSay(1));
+        assertEquals(s2, test.countAndSay(2));
+        assertEquals(s3, test.countAndSay(3));
+        assertEquals(s4, test.countAndSay(4));
+        assertEquals(s5, test.countAndSay(5));
+
+    }
+}
