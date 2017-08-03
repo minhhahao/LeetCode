e788fcc5d9f0469df05cee9ffa069781f298d76c
HazzaCheng <hazzacheng@gmail.com>
2017-08-03 12:49:23 +0800

    add: No51

---
+++ src/main/java/com/hazza/LeetCodeByJava/No51NQueens.java
@@ -0,0 +1,61 @@
+package com.hazza.LeetCodeByJava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/n-queens/description/
+ */
+public class No51NQueens {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        if (n <= 0) return res;
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+
+        placeQueens(res, board, 0);
+
+        return res;
+    }
+
+    private void placeQueens(List<List<String>> res, char[][] board, int colIndex) {
+        int len = board.length;
+        if (colIndex == len) {
+            res.add(construct(board));
+            return;
+        }
+        for (int i = 0; i < len; i++) {
+            if (isSafe(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                placeQueens(res, board, colIndex + 1);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int rowIndex, int colIndex) {
+        int len = board.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < colIndex; j++) {
+                if (board[i][j] == 'Q' &&
+                        (rowIndex == i ||
+                                rowIndex - colIndex == i - j ||
+                                rowIndex + colIndex == i + j))
+                    return false;
+            }
+        }        
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        int len = board.length;
+        for (int i = 0; i < len; i++)
+            res.add(new String(board[i]));
+
+        return res;
+    }
+
+}
---
+++ src/test/java/com/hazza/LeetCodeByJava/No51Test.java
@@ -0,0 +1,46 @@
+package com.hazza.LeetCodeByJava;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by hazza on 8/3/17.
+ */
+public class No51Test {
+    No51NQueens test;
+
+    private String printListList(List<List<String>> lists) {
+        StringBuilder sb = new StringBuilder();
+        for (List<String> l: lists)
+            for (String s: l)
+                sb.append(s);
+        return sb.toString();
+    }
+
+
+    @Before
+    public void setUp() {
+        test = new No51NQueens();
+    }
+
+    @Test
+    public void testFourQueens() {
+        List<List<String>> righgList = new ArrayList<>();
+        righgList.add(Arrays.asList(".Q..", "...Q", "Q...", "..Q."));
+        righgList.add(Arrays.asList("..Q.", "Q...", "...Q", ".Q.."));
+        String right = printListList(righgList);
+        for (List<String> l: test.solveNQueens(4)) {
+            for (String s : l)
+                System.out.println(s);
+            System.out.println();
+        }
+        String res = printListList(test.solveNQueens(4));
+        assertEquals(right, res);
+    }
+}
