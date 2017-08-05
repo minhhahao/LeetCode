408ea74c1b293f223d04b9d5751735c1d89d3438
HazzaCheng <hazzacheng@gmail.com>
2017-08-05 10:11:30 +0800

    add: No52

---
+++ src/test/java/com/hazza/LeetCodeByJava/No52Test.java
@@ -0,0 +1,28 @@
+package com.hazza.LeetCodeByJava;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by hazza on 8/5/17.
+ */
+public class No52Test {
+    No52NQueensII test;
+
+    @Before
+    public void setUp() {
+        test = new No52NQueensII();
+    }
+
+    @Test
+    public void test4Queens() {
+        assertEquals(2, test.totalNQueens(4));
+    }
+
+    @Test
+    public void test8Queens() {
+        assertEquals(92, test.totalNQueens(8));
+    }
+}
