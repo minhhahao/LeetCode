fafb6db91e7f55f7daf9866c6f0b75a0ed1919fe
HazzaCheng <hazzacheng@gmail.com>
2017-08-21 11:33:35 +0800

    add: No67

---
+++ src/test/java/com/hazza/LeetCodeByJava/No67Test.java
@@ -0,0 +1,37 @@
+package com.hazza.LeetCodeByJava;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Author: HazzaCheng
+ * Contact: hazzacheng@gmail.com
+ * Date: 2017-08-21
+ * Time: 11:03 AM
+ */
+public class No67Test {
+    No67AddBinary test;
+
+    @Before
+    public void setUp() {
+        test = new No67AddBinary();
+    }
+
+    @Test
+    public void testSimple() {
+        assertEquals("1010", test.addBinary("111", "11"));
+        assertEquals("111", test.addBinary("100", "11"));
+        assertEquals("111", test.addBinary("11", "100"));
+        assertEquals("1000", test.addBinary("101", "11"));
+        assertEquals("100", test.addBinary("11", "1"));
+    }
+
+    @Test
+    public void testLong() {
+        assertEquals("110001", test.addBinary("101111", "10"));
+    }
+}
