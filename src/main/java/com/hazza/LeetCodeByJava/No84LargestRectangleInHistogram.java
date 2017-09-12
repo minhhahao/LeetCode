7a9005981fbe110971e19f921548df60fc1caa3e
HazzaCheng <hazzacheng@gmail.com>
2017-09-12 12:18:53 +0800

    add: No84

--- src/main/java/com/hazza/LeetCodeByJava/No84LargestRectangleInHistogram.java
+++ src/main/java/com/hazza/LeetCodeByJava/No84LargestRectangleInHistogram.java
@@ -1,5 +1,10 @@
 package com.hazza.LeetCodeByJava;
 
+import com.hazza.LeetCodeByJava.datastructure.SegmentTree;
+import org.omg.CORBA.MARSHAL;
+
+import java.util.Stack;
+
 /**
  * Created with IntelliJ IDEA.
  * Description: https://leetcode.com/problems/largest-rectangle-in-histogram/description/
@@ -9,11 +14,39 @@ package com.hazza.LeetCodeByJava;
  * Time: 6:04 PM
  */
 public class No84LargestRectangleInHistogram {
+
+    // solution 1
     public int largestRectangleArea(int[] heights) {
-        int res = 0, left = 0, right = heights.length - 1;
-        while (left != right) {
-            res = Math.max(res, (right - left) * Math.min(heights[left], heights[right]));
-            if (heights[left] < heights[right]) ++left;
+        if (heights == null || heights.length == 0) return 0;
+        int[] st = SegmentTree.constructST(heights);
+        return getAreaRec(heights, st, 0, heights.length - 1);
+    }
+
+    private int getAreaRec(int[] heights, int[] st, int left, int right) {
+        if (left > right) return Integer.MIN_VALUE;
+        if (left == right) return heights[left];
+        int m = SegmentTree.RMQ(heights, st, left, right);
+        return Math.max(Math.max(getAreaRec(heights, st, left, m - 1),
+                getAreaRec(heights, st, m + 1, right)),
+                (right - left + 1) * heights[m]);
+    }
+
+    // solution 2
+    public int largestRectangleArea1(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0, n = heights.length;
+
+        for (int i = 0; i <= n ; i++) {
+            int top = (i == n ? 0 : heights[i]);
+            if (stack.isEmpty() || heights[stack.peek()] <= top) stack.push(i);
+            else {
+                top = stack.pop();
+                maxArea = Math.max(maxArea,
+                        heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+                --i;
+            }
         }
+
+        return maxArea;
     }
 }
--- src/test/java/No84Test.java
+++ src/test/java/No84Test.java
@@ -2,6 +2,8 @@ import com.hazza.LeetCodeByJava.No84LargestRectangleInHistogram;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 import static junit.framework.TestCase.assertEquals;
 
 /**
@@ -15,20 +17,42 @@ import static junit.framework.TestCase.assertEquals;
 public class No84Test {
     No84LargestRectangleInHistogram test;
 
+    private int[] creatRandomNums(int len) {
+        int[] res = new int[len];
+        Random random = new Random();
+        for (int i = 0; i < len; i++) res[i] = random.nextInt(100) + 1;
+        return res;
+    }
+
     @Before
     public void setUp() {
         test = new No84LargestRectangleInHistogram();
     }
 
     @Test
-    public void test1() {
-        int[] heights = {6, 2, 5, 4, 5, 2, 6};
-        assertEquals(12, test.largestRectangleArea(heights));
+    public void testShort() {
+        int[] heights = {1, 1, 1, 1, 1};
+        assertEquals(5, test.largestRectangleArea(heights));
+        assertEquals(5, test.largestRectangleArea1(heights));
     }
 
     @Test
-    public void test2() {
+    public void testFixed1() {
+        int[] heights = {6, 2, 5, 4, 5, 2, 1};
+        assertEquals(12, test.largestRectangleArea(heights));
+        assertEquals(12, test.largestRectangleArea1(heights));
+    }
+
+    @Test
+    public void testFixed2() {
         int[] heights = {2, 1, 5, 6, 2, 3};
         assertEquals(10, test.largestRectangleArea(heights));
+        assertEquals(10, test.largestRectangleArea1(heights));
+    }
+
+    @Test
+    public void testRandom() {
+        int[] heights = creatRandomNums(100);
+        assertEquals(test.largestRectangleArea(heights), test.largestRectangleArea1(heights));
     }
 }
