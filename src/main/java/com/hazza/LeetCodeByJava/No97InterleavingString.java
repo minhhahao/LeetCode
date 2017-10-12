881305e6b57322157d41d4cfc21b22e62a028528
HazzaCheng <hazzacheng@gmail.com>
2017-10-12 19:00:43 +0800

    add: No97

---
+++ src/main/java/com/hazza/LeetCodeByJava/No97InterleavingString.java
@@ -0,0 +1,29 @@
+package com.hazza.LeetCodeByJava;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: https://leetcode.com/problems/interleaving-string/description/
+ * Author: HazzaCheng
+ * Contact: hazzacheng@gmail.com
+ * Date: 2017-10-12
+ * Time: 9:12 AM
+ */
+public class No97InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 + len2 != s3.length()) return false;
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0 && j == 0) dp[0][0] = true;
+                else if (i == 0) dp[i][j] = dp[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                else if (j == 0) dp[i][j] = dp[i- 1][j] && (s1.charAt(i - 1) == s3.charAt(i + j - 1));
+                else dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+                            || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+
+        return dp[len1][len2];
+    }
+}
