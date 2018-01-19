c7fd728272278ef0fe25e9330414e1dbdeba056e
HazzaCheng <hazzacheng@gmail.com>
2018-01-19 14:38:28 +0800

    add No107

---
+++ src/main/java/com/hazza/LeetCodeByJava/SegmentTwo/No107BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,57 @@
+package com.hazza.LeetCodeByJava.SegmentTwo;
+
+import com.hazza.LeetCodeByJava.datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ * User: HazzaCheng
+ * Contact: hazzacheng@gmail.com
+ * Date: 18-1-19
+ * Time: 2:23 PM
+ */
+public class No107BinaryTreeLevelOrderTraversalII {
+    // solution 2
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            res.add(0, level);
+        }
+
+        return res;
+    }
+
+    // solution 3
+    public List<List<Integer>> levelOrderBottom1(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        levelHelper(res, root, 0);
+
+        return res;
+    }
+
+    private void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= res.size()) res.add(0, new ArrayList<>());
+        res.get(res.size() - height - 1).add(root.val);
+        levelHelper(res, root.left, height + 1);
+        levelHelper(res, root.right, height + 1);
+    }
+
+}
