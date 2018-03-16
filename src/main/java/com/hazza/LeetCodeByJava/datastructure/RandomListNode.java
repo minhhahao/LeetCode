e55455d6176726b3a4f5b55bfb8136931747fd9b
HazzaCheng <hazzacheng@gmail.com>
2018-03-16 22:30:24 +0800

    add No138

---
+++ src/main/java/com/hazza/LeetCodeByJava/datastructure/RandomListNode.java
@@ -0,0 +1,15 @@
+package com.hazza.LeetCodeByJava.datastructure;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: HazzaCheng
+ * Contact: hazzacheng@gmail.com
+ * Date: 18-3-16
+ * Time: 10:10 PM
+ */
+public class RandomListNode {
+ public int label;
+ public RandomListNode next, random;
+ public RandomListNode(int x) { this.label = x; }
+}
