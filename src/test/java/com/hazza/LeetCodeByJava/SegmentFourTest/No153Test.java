c6b9318375c91cc489c2e9f44002050a4a344c12
HazzaCheng <hazzacheng@gmail.com>
2018-04-09 09:25:59 +0800

    add No153

---
+++ src/test/java/com/hazza/LeetCodeByJava/SegmentFourTest/No153Test.java
@@ -0,0 +1,32 @@
+package com.hazza.LeetCodeByJava.SegmentFourTest;
+
+import com.hazza.LeetCodeByJava.SegmentFour.No153FindMinimumInRotatedSortedArray;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author Hazzacheng
+ * Contact: hazzacheng@gmail.com
+ * Date: 18-4-9
+ * Time: 9:17 AM
+ */
+public class No153Test {
+    No153FindMinimumInRotatedSortedArray test;
+
+    @Before
+    public void setUp() {
+        test = new No153FindMinimumInRotatedSortedArray();
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+
+        assertEquals(0, test.findMin(nums));
+    }
+}
