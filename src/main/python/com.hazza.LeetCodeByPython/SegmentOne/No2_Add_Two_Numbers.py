b3bb6d046af5803c16480e153dad6285ec60f502
HazzaCheng <hazzacheng@gmail.com>
2019-04-05 18:53:22 +0800

    add no.2

--- src/main/python/com.hazza.LeetCodeByPython/SegmentOne/No2_Add_Two_Numbers.py
+++ src/main/python/com.hazza.LeetCodeByPython/SegmentOne/No2_Add_Two_Numbers.py
@@ -6,23 +6,25 @@
 from ListNode import ListNode, makeListNode, printListNode
 
 
-def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-    res = ListNode(0)
-    tmp = res
-    left = 0
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode(0)
+        tmp = res
+        left = 0
 
-    while l1 or l2 or left:
-        left, right = divmod(sum(l and l.val or 0 for l in (l1, l2)) + left, 10)
-        tmp.next = ListNode(right)
-        tmp = tmp.next
-        l1 = l1 and l1.next
-        l2 = l2 and l2.next
+        while l1 or l2 or left:
+            left, right = divmod(sum(l and l.val or 0 for l in (l1, l2)) + left, 10)
+            tmp.next = ListNode(right)
+            tmp = tmp.next
+            l1 = l1 and l1.next
+            l2 = l2 and l2.next
 
-    return res.next
+        return res.next
 
 
 if __name__ == '__main__':
+    solution = Solution()
     l1 = makeListNode([2, 4, 3])
     l2 = makeListNode([5, 6, 4])
-    res = addTwoNumbers(None, l1, l2)
+    res = solution.addTwoNumbers(l1, l2)
     printListNode(res)
