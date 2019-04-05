d7593a47aca0a39158043910552fd4437e29df95
HazzaCheng <hazzacheng@gmail.com>
2019-04-05 18:49:40 +0800

    add no.3

--- src/main/python/com.hazza.LeetCodeByPython/SegmentOne/No3_Longest_Substring_Without_Repeating_Characters.py
+++ src/main/python/com.hazza.LeetCodeByPython/SegmentOne/No3_Longest_Substring_Without_Repeating_Characters.py
@@ -4,21 +4,23 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 
-def lengthOfLongestSubstring(self, s: str) -> int:
-    maxLen = 0
-    start = i = 0
-    used = {}
-    for i in range(len(s)):
-        if s[i] in used and start <= used[s[i]]:
-            start = used[s[i]] + 1
-        else:
-            maxLen = max(maxLen, i - start + 1)
-        used[s[i]] = i
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        maxLen = 0
+        start = i = 0
+        used = {}
+        for i in range(len(s)):
+            if s[i] in used and start <= used[s[i]]:
+                start = used[s[i]] + 1
+            else:
+                maxLen = max(maxLen, i - start + 1)
+            used[s[i]] = i
 
-    return maxLen
+        return maxLen
 
 
 if __name__ == '__main__':
-    assert lengthOfLongestSubstring(None, "abcabcbb") == 3
-    assert lengthOfLongestSubstring(None, "bbbbb") == 1
-    assert lengthOfLongestSubstring(None, "pwwkew") == 3
+    solution = Solution()
+    assert solution.lengthOfLongestSubstring("abcabcbb") == 3
+    assert solution.lengthOfLongestSubstring("bbbbb") == 1
+    assert solution.lengthOfLongestSubstring("pwwkew") == 3
