10cdc4305113dc3aa5140e964b7a3aeb0621f612
HazzaCheng <hazzacheng@gmail.com>
2019-04-29 22:11:17 +0800

    add no.34

--- src/main/python/com/hazza/LeetCodeByPython/SegmentOne/No34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ src/main/python/com/hazza/LeetCodeByPython/SegmentOne/No34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -37,5 +37,6 @@ class Solution:
 
 if __name__ == '__main__':
     solution = Solution()
+    
     assert solution.searchRange([5, 7, 7, 8, 8, 10], 8) == [3, 4]
     assert solution.searchRange([5, 7, 7, 8, 8, 10], 6) == [-1, -1]
