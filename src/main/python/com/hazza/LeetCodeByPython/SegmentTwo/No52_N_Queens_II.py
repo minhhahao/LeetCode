0213d706ee37f9e5acf9ba982ef2a2f43f3a3403
HazzaCheng <hazzacheng@gmail.com>
2019-05-23 20:41:25 +0800

    add no.52

---
+++ src/main/python/com/hazza/LeetCodeByPython/SegmentTwo/No52_N_Queens_II.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by HazzaCheng on 2019/5/23 20:40
+# https://leetcode.com/problems/n-queens-ii/
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+
+
+if __name__ == '__main__':
+    pass
