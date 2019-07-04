7ba07b423fccf13cfa2e6b742f2dc46bddff59b9
HazzaCheng <hazzacheng@gmail.com>
2019-07-04 09:04:34 +0800

    add no.72

--- src/main/java/com/hazza/LeetCodeByJava/SegmentTwo/No72EditDistance.java
+++ src/main/java/com/hazza/LeetCodeByJava/SegmentTwo/No72EditDistance.java
@@ -41,21 +41,20 @@ public class No72EditDistance {
 
         for (int i = 0; i <= n; i++) dp[i] = i;
         for (int i = 1; i <= m; i++) {
-            int prev = i;
+            int prev = dp[0];
             for (int j = 1; j <= n; j++) {
-                int cur;
+                int temp = dp[j];
                 if (word1.charAt(i - 1) == word2.charAt(j - 1))
-                    cur = dp[j - 1];
+                    dp[j] = prev;
                 else {
                     int insert = prev;
                     int delete = dp[j];
                     int replace = dp[j - 1];
-                    cur = Math.min(Math.min(insert, delete), replace) + 1;
+                    dp[j] = Math.min(Math.min(insert, delete), replace) + 1;
                 }
-                dp[j - 1] = prev;
-                prev = cur;
+                prev = temp;
             }
-            dp[n] = prev;
+            dp[0] = i;
         }
 
         return dp[n];
