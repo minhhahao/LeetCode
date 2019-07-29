be7e4c2d88bb0b4c2492214183d16ddf07a18b3d
HazzaCheng <hazzacheng@gmail.com>
2019-07-29 09:53:11 +0800

    add no.88

--- src/test/python/two_pointers/test_no88.py
+++ src/test/python/two_pointers/test_no88.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.solution = Solution()
 
-    def test1(self):
+    def test(self):
         nums1 = [1, 2, 3, 0, 0, 0]
         nums2 = [2, 5, 6]
         self.solution.merge(nums1, 3, nums2, 3)
